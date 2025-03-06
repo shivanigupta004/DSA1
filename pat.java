9d24fff03f9e01ae84c9b1c494aa334d4f6c477b
shivanigupta004 <94shivani004@gmail.com>
2025-03-06 09:52:57 +0000

    added

--- pat.java
+++ pat.java
@@ -11,7 +11,7 @@ public class pat {
             }
             System.out.println();
         }
-
+        System.out.println();
         int row = 5;
         
         for (int i1 = 1; i1 <= row; i1++) {
@@ -20,6 +20,18 @@ public class pat {
             }
             for (int k1 = 1; k1 <= 2* i1 - 1; k1++) {
                 System.out.print(i1);
+            }
+            System.out.println();
+        } 
+        System.out.println();
+        int r = 5;
+        
+        for (int i2 = r; i2 >= 1; i2--) {
+            for (int j2 = r - i2; j2 > 0; j2--) {
+                System.out.print(" ");
+            }
+            for (int k2 = 1; k2 <= 2* i2 - 1; k2++) {
+                System.out.print(i2);
             }
             System.out.println();
         } 
