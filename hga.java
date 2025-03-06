c2be4dd0191b0b6054697f246e47940801776895
shivanigupta004 <94shivani004@gmail.com>
2025-03-06 09:02:55 +0000

    added

---
+++ hga.java
@@ -0,0 +1,38 @@
+public class hga{
+    public static void main(String[]args){
+        /*  int k=1;
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++)  {
+                System.out.print(k++);
+                if(k>9){
+                    k=1;
+                }
+                }System.out.print("\n");
+            }*/
+            /*
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=7;j++){
+                     if(i==2||i==4||i+j==5||j-i==3||i-j==1){
+                        System.out.print("*");
+                     }else{
+                        System.out.print(" ");
+                     }
+                }System.out.println();
+        }*/
+        /* 
+        for(int i=1;i<=5;i++){
+           for(int j=5;j>=i;j--){ 
+                System.out.print(j%2);
+            }System.out.println();
+        }*/
+         
+        for(char i='E';i>='A';i--){
+            for(char j='A';j<=i;j++){ 
+                 System.out.print(j);
+             }System.out.println();
+         }
+         
+
+        
+}
+} 
