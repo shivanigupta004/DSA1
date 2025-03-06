4c082ce60aed28ed94d16d2c4d8f6bff5b99aea2
shivanigupta004 <94shivani004@gmail.com>
2025-03-06 09:07:39 +0000

    added

---
+++ tri.java
@@ -0,0 +1,13 @@
+public class tri {
+    public static void main(String[]args){
+          int k=1;
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++)  {
+                System.out.print(k++);
+                if(k>9){
+                    k=1;
+                }
+                }System.out.print("\n");
+            }
+    
+}
