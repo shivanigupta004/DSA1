f20f8143a3a5be93da1db2e3d43d3413818724a5
shivanigupta004 <94shivani004@gmail.com>
2025-03-06 09:18:22 +0000

    added

--- tri.java
+++ tri.java
@@ -8,6 +8,16 @@ public class tri {
                     k=1;
                 }
                 }System.out.print("\n");
-            }   
+            } 
+            
+            for(int i=1;i<=5;i++){
+                for(int j=1;j<=7;j++){
+                     if(i==2||i==4||i+j==5||j-i==3||i-j==1){
+                        System.out.print("*");
+                     }else{
+                        System.out.print(" ");
+                     }
+                }System.out.println();
+        }  
 }
 }
