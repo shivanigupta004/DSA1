137d4b6c4747444822e63e86bb02bffc2c263319
shivanigupta004 <94shivani004@gmail.com>
2025-03-06 09:42:12 +0000

    added

--- pat.java
+++ pat.java
@@ -1,12 +1,18 @@
 public class pat {
-    public static void man(String[] args) {
-        int i;
-        int j;
-        for(i=1;i<=5;i++){
-            for(j=1;j<=5;j++){
-            System.out.print("*");
-        }System.out.println( );
-    } 
-} 
+    public static void main(String[] args) {
+        int rows = 5;
+    
+        for (int i = 1; i <= rows; i++) {
+            for (int j = rows - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2* i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+           
+    }  
     
 }
