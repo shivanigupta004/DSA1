49c38345ccee56f29987db435bd20dc459e041b9
shivanigupta004 <94shivani004@gmail.com>
2025-03-12 05:09:12 +0000

    added

--- linearsearch.java
+++ linearsearch.java
@@ -1,3 +1,18 @@
 public class linearsearch {
+    public static int linSearch(int []nums,int key){
+
+       for(int i=0;i<nums.length;i++){
+        if(nums[i]==key){
+            return i;
+        }
+       }return -1;
+    }
+    public static void main(String[]args){
+        int nums[]={2,4,6,8,10,12,14,16,18,20};
+        int key=10;
+        int in=linSearch(nums,key);
+        System.out.println("Key is found at index "+in);
+
+    }
     
 }
