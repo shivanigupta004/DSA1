bf8bd758f0e07ec110ced7826cada5d6a6360126
shivanigupta004 <94shivani004@gmail.com>
2025-03-13 06:01:21 +0000

    added

--- pair.java
+++ pair.java
@@ -1,13 +1,15 @@
 public class pair {
     public static void pairs(int arr[]){
+        int count=0;
         for(int i=0;i<arr.length;i++){
             int current=arr[i];
             
             for(int j=i+1;j<arr.length;j++){
                 int c=arr[j];
                 System.out.print("("+current+","+c+")");
+                count++;
             }  System.out.println();
-        }
+        }System.out.println("Total no of pairs is "+count);
     }
     public static void main(String args[]){
         int arr[]={2,4,6,8,10};
