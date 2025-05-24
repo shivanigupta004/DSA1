5cb1257798dde40a4329404fde8477f11b538896
shivanigupta004 <94shivani004@gmail.com>
2025-05-24 17:08:45 +0000

    Added bubble sort program

---
+++ bubblesort.java
@@ -0,0 +1,29 @@
+public class bubblesort {
+    public static void bubblesorting(int[] arr) {
+
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+            }
+        }
+    }
+}
+    public static void printArr(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 3, 2, 1 };
+        System.out.print("Before Sorting arr : ");
+        printArr(arr);
+        bubblesorting(arr);
+        System.out.print("After  Sorting arr : ");
+        printArr(arr);
+    }
+}
