5d1dea06ebf9265914241be2954774255f837980
shivanigupta004 <94shivani004@gmail.com>
2025-05-25 14:04:44 +0000

    Added inbuilt sorting technique

--- inbuiltSorting.java
+++ inbuiltSorting.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 public class inbuiltSorting {
 
@@ -7,11 +8,23 @@ public class inbuiltSorting {
             System.out.print(ele + " ");
         }System.out.println();
     }
+
+    public static void printArr(Integer[] arra) {
+        for (Integer ele : arra) {
+            System.out.print(ele + " ");
+        }System.out.println();
+    }
     public static void main(String[] args) {
         int arr []= {1,9,2,8,3,7,4,5,6};
+        Integer arra[] = {1,9,2,8,3,7,4,5,6};
+
         Arrays.sort(arr,0,6);
         printArr(arr);
+        
         Arrays.sort(arr);
         printArr(arr);
+
+        Arrays.sort(arra,Collections.reverseOrder());
+        printArr(arra);
     }
 }
