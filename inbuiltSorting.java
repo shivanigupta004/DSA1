4fc545986a15f6a7eb5b9dcb2f4f7eee95f044dc
shivanigupta004 <94shivani004@gmail.com>
2025-05-25 13:54:19 +0000

    Added inbuilt sorting technique

--- inbuiltSorting.java
+++ inbuiltSorting.java
@@ -9,6 +9,8 @@ public class inbuiltSorting {
     }
     public static void main(String[] args) {
         int arr []= {1,9,2,8,3,7,4,5,6};
+        Arrays.sort(arr,0,6);
+        printArr(arr);
         Arrays.sort(arr);
         printArr(arr);
     }
