b8dfc41f40a2efbc33a3f8a3aea763ed682b5154
shivanigupta004 <94shivani004@gmail.com>
2025-05-28 14:36:30 +0000

    added transpose of a matrix

--- transpose.java
+++ transpose.java
@@ -11,18 +11,28 @@ public class transpose {
 
     }
     public static void PrintArr(int transposeMatrix[][] ) {
+        int sum = 0, count = 0;
        for(int i=0;i<transposeMatrix.length;i++){
-            for(int j=0;j<transposeMatrix[0].length;j++){
+            for(int j=0;j<transposeMatrix[0].length;j++)
+            {
               System.out.print( transposeMatrix[i][j] +" ");
+              if(i==1){
+                sum += transposeMatrix[i][j];
+              }else if(transposeMatrix[i][j] == 7){
+                count++;
+              }
             }
             System.out.println();
-        }  
+        }  System.out.println("sum of 2nd row elements is : "+ sum); 
+        System.out.println("No of 7 is : "+ count); 
     }
+
    
     public static void main(String[] args) {
         int[][] arr = { {1,2,3},{4,5,6},{7,8,9} };
         int[][] transposeMatrix = new int[arr.length][arr[0].length];
         transposeOfMatrix(arr,transposeMatrix);
+        System.out.println("Transpose Matrix of nums is : "); 
         PrintArr(transposeMatrix);
     }
     
