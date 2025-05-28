0e1101108176408d26f0a3b3241f1654d1a98d0d
shivanigupta004 <94shivani004@gmail.com>
2025-05-28 14:10:44 +0000

    search in sorted matrix(starircase)program

---
+++ SortedMatrix.java
@@ -0,0 +1,22 @@
+public class SortedMatrix {
+    public static void SearchInSortedMatrix(int matrix[][],int key) {
+        int row = 0, col = matrix[0].length-1;
+        //Also known as staircase search
+        while(row<matrix.length && col>=0){
+            if(matrix[row][col] == key){
+                System.out.println("Found  key at ("+row+","+col+") in the matrix .");  
+                return ;
+            }else if(key < matrix[row][col] ){
+                col--;
+            }else{
+                row++;
+            }
+        }System.out.println("Key not found in the matrix.");  
+        return;
+    }
+    public static void main(String[] args) {
+        int matrix [] [] = {{10,20,30,40},{15,25,35,45},{27,29,37,48},{32,33,39,50}};
+        int key = 39;
+        SearchInSortedMatrix( matrix,key);
+    }
+}
