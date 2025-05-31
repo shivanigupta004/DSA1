6cec92921d18fadec718298c3b548255b0ed8d08
shivanigupta004 <94shivani004@gmail.com>
2025-05-31 06:50:19 +0000

    To check palindrome of string

--- palinString.java
+++ palinString.java
@@ -1,9 +1,15 @@
 public class palinString {
-    public static void palindrome(String str){
-        for(int i = 0 ;i < )
+    public static boolean palindrome(String str) {
+        for (int i = 0; i < (str.length() / 2); i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
     }
+
     public static void main(String[] args) {
-        
+        String str = "racecar";
+        System.out.println(palindrome(str));
     }
-    
 }
