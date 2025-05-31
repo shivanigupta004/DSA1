ea516525dfa2ef84d4d3a358e0259f96f085a93a
shivanigupta004 <94shivani004@gmail.com>
2025-05-31 15:10:02 +0000

    lexicographir comparisions

--- LrgStr.java
+++ LrgStr.java
@@ -4,12 +4,14 @@ public class LrgStr {
 
     public static void main(String[] args) {
         String fruits[] = {"apple", "mango", "banana"};
-         String largest = fruits[0];
-         for(int i=1; i<fruits.length; i++){
-           if( largest.compareTo(fruits[i]) < 0){
-             largest = fruits[i];
+        String largest = fruits[0];
+        int r = 0;
+        for(int i=1; i<fruits.length; i++){
+            if( largest.compareTo(fruits[i]) < 0){
+               largest = fruits[i];
+               r = i;
            }
-         }System.out.println(largest);
+        }System.out.println("largest is : "+largest+"\nAnd its index is : "+r);
     }
 
 }
