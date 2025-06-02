ce21f873aa00f9ce6703ba329b0565364ed73202
shivanigupta004 <94shivani004@gmail.com>
2025-06-02 16:50:04 +0000

    no is power of 2 and count of 1

--- bit4.java
+++ bit4.java
@@ -2,7 +2,19 @@ public class bit4 {
     public static boolean isPowerOfTwo(int n){
         return (n & (n-1)) == 0;
     }
+    public static int countSetBits(int n) { 
+        //O(log n)  no of one's
+        //n = log n + 1 bits
+        int count = 0;
+        while(n>0) {
+            if((n & 1) !=0){ //check our lsb
+               count++;
+            }
+            n = n>>1;
+        }return count;
+    }
     public static void main(String[] args) {
-        System.out.println(isPowerOfTwo(15));
+        System.out.println(isPowerOfTwo(16));
+        System.out.println(countSetBits(15));
     }
 }
