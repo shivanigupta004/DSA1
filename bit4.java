dd84bc09de814d3550bd53f5b34107d5ad40a5a7
shivanigupta004 <94shivani004@gmail.com>
2025-06-04 05:13:35 +0000

    fast exponential'

--- bit4.java
+++ bit4.java
@@ -13,8 +13,20 @@ public class bit4 {
             n = n>>1;
         }return count;
     }
+    public static int fastExponential(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) { //check lsb
+                ans *= a;
+            }
+            a = a * a;
+            n = n>>1;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         System.out.println(isPowerOfTwo(16));
         System.out.println(countSetBits(15));
+        System.out.println(fastExponential(5, 3));
     }
 }
