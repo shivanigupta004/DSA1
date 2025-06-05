a618446dac6e1a9d690b3db3c87fccc52f0e933b
shivanigupta004 <94shivani004@gmail.com>
2025-06-05 16:46:38 +0000

    method overloading & overriding

--- OOps3.java
+++ OOps3.java
@@ -8,8 +8,17 @@ public class OOps3 {
      dobby.legs = 4;
      System.out.println(dobby.legs);
      */
-
-     
+/*   
+     Calculator cal = new Calculator();
+     System.out.println(cal.sum(9,3));
+     System.out.println(cal.sum(2.4f,3.6f));
+     System.out.println(cal.sum(5,3,4));    
+     */
+    //runtime polymorphism- method overriding
+     Fish f = new Fish();
+     f.eat();
+     Animal a = new Animal();
+     a.eat();
     }
 }
 
@@ -28,6 +37,9 @@ class Animal {
 //Derived class - single level inheritance
 class Fish extends Animal{
    int fins;
+   void eat(){
+      System.out.println("Fish eat fishfood.");
+   }
    void swim(){
     System.out.println("Fish Swims in water.");
    }
@@ -51,3 +63,18 @@ class bird extends Animal {
    }
 }
 // all together(above) = hybrid inheritane
+
+//method overloading - compiletime polymorphism
+class Calculator{
+   int sum(int a, int b){
+      return a + b;
+   }
+   float sum(float a, float b){
+      return a + b;
+   }
+   int sum(int a, int b, int c){
+      return a + b + c;
+   }
+}
+//method overriding = Animal & Fish class --> eat() 
+//same fuction in both parent and child class
