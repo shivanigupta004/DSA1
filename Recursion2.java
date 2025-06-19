6c11fa2a4e13ac0c7ae7697983089a792d4d1ee3
shivanigupta004 <94shivani004@gmail.com>
2025-06-19 09:31:35 +0000

    friends problem

--- Recursion2.java
+++ Recursion2.java
@@ -5,7 +5,7 @@ public class Recursion2 {
         if(n==0 || n==1){
             return 1;
         }
-        //kaam
+        
         //vertical tiles choice
         int fnm1 = tilingProblem(n - 1);
 
@@ -33,11 +33,37 @@ public class Recursion2 {
             removeDuplicates(str, idx +1, newStr.append(currChar), map);
         }
     }
+
+    
+    public static int friendsPairing(int n) { //2 x n (floor size)
+        //base case
+        if(n==1 || n==2){
+            return n;
+        }
+        /* 
+        //single choice
+        int fnm1 = friendsPairing(n - 1);
+
+        //pair choice
+        int fnm2 = friendsPairing(n - 2);
+        int pairWays = (n-1) * fnm2;
+
+        int totWays = fnm1 + pairWays;
+        return totWays;   */
+        
+        return friendsPairing(n-1) + (n-1) * friendsPairing(n-2);
+    }
+
+
+
     public static void main(String[] args) {
         //System.out.println(tilingProblem(4));
 
-        String str = "appnnacollege";
-        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
+      /*   String str = "appnnacollege";
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);  */
+
+        int r = friendsPairing(3);
+        System.out.println(r);
     }
 }
 
