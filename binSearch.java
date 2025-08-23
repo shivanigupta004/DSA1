45d4bb4cf52b410dfcd0a3501674ff77920be0a3
Shivani Gupta <94shivani004@gmail.com>
2025-08-23 17:11:41 +0000

    Added binary search implementation

--- binSearch.java
+++ binSearch.java
@@ -1,23 +1,26 @@
 public class binSearch {
     public static int binarySearch(int arr[],int key){
-       int start=0 ,end=arr.length-1;
+       int start = 0, end = arr.length-1;
 
-       while(start<=end){
-        int mid=(start+end)/2;//find mid
-         //comparisons
-        if(arr[mid]==key){   //mid
+       while(start <= end)
+       {
+        int mid = (start + end) / 2;   //finding mid
+ 
+        //Comparisons
+        if(arr[mid] == key){    //Mid
             return mid;
-        }if(arr[mid]<key){   //right
-            start=mid+1;
-        }else{               //left
-             end=mid-1;
+        }if(arr[mid] < key){ //Right
+            start = mid + 1;   //Update start
+        }else{              //Left
+             end = mid - 1;    //Update end
         }
-       } return -1;
+       }return -1;
+
     }
     public static void main(String[]args){
-     int arr[]={2,4,6,8,10,12,14};
+     int arr[]={2,4,6,8,10,12,14,16,18};
      int key=10;
-     System.out.println("index of key is "+binarySearch(arr,key));
+     System.out.println("Index of key is : " + binarySearch(arr , key));
     }
     
 }
