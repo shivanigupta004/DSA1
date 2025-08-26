b4e234065a59e149e1f05aede1e19f7e2044236e
Shivani Gupta <94shivani004@gmail.com>
2025-08-26 09:38:43 +0000

    Add prefix sum approach for max subarray sum in Java

--- PrefixSumOfSubArry.java
+++ PrefixSumOfSubArry.java
@@ -1,38 +1,36 @@
 public class PrefixSumOfSubArry {
-    public static void subArray(int arr[]){
-       int prefix[]=new int[arr.length];
-       int currsum=0;
-       int minsum=Integer.MAX_VALUE;
-       int maxsum=Integer.MIN_VALUE;
+    public static void subArray(int arr[])
+    {
+       int prefix [] = new int[arr.length];
+       int currsum = 0;
+       int maxsum = Integer.MIN_VALUE;
+       prefix[0] = arr[0];
+        //Calculate prefix sum
+        for(int i = 1; i < prefix.length; i++)
+        {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
 
-          prefix[0]=arr[0];
-          //calculate prefix sum
-       for(int p=1;p<prefix.length;p++){
-            prefix[p]=prefix[p-1]+arr[p];
-       }
-
-        for(int i=0;i<arr.length;i++)
-        {   int start=i;
-            for(int j=i;j<arr.length;j++)
-            {   int end=j;
-
-                currsum = start==0 ? prefix[end] : prefix[end]-prefix[end-1];
-                
-                if(maxsum<currsum){
-                    maxsum=currsum;
-                }
-                if(minsum>currsum){
-                    minsum=currsum;
+        for(int i = 0; i < arr.length; i++)
+        {   
+            int start = i;
+            for(int j = i; j < arr.length; j++)
+            {   
+                int end = j;
+                //Calculate current sum
+                currsum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+                if(maxsum < currsum)
+                {
+                    maxsum = currsum;
                 }
             }
         }
-        System.out.println("max sum of subarrays is "+maxsum);
-        System.out.println("min sum of subarrays is "+minsum);
+        System.out.println("Maximum sum is: " + maxsum);
     }
     
-    public static void main(String args[]){
-        int arr[]={1,-2,6,-1,3};
+    public static void main(String args [] )
+    {
+        int arr[] = { 1,-2,6,-1,3 };
         subArray(arr);
     }
-    
 }
