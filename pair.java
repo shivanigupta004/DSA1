c8aceee976f62bc1a49e3bef1f5866f422b45c40
Shivani Gupta <94shivani004@gmail.com>
2025-08-26 06:10:33 +0000

    Add pair generation logic in Java

--- pair.java
+++ pair.java
@@ -1,23 +1,25 @@
 public class pair {
-    public static void pairs(int arr[]){
-        int count=0;
-        for(int i=0;i<arr.length;i++){
-            int current=arr[i];
-            
-            for(int j=i+1;j<arr.length;j++){
-                int c=arr[j];
-                System.out.print("("+current+","+c+")");
+    public static void pairs( int arr [] )
+    {
+        int count = 0;
+        for(int i = 0;i < arr.length; i++)
+        {
+            for(int j = i+1; j < arr.length; j++) 
+            {
+                System.out.print("("+arr[i]+","+arr[j]+")");
                 count++;
-            }  System.out.println();
-        }System.out.println("Total no of pairs is "+count);
+            }  
+            System.out.println();
+        }
+        System.out.println("Total no of pairs is "+count);
     }
-    public static void main(String args[]){
+    public static void main( String args [] )
+    {
         int arr[]={2,4,6,8,10};
-        int arr2[]={1,2,3,4,5,6,7,8,9,10};
+        int arr2[]={1,2,4,6,8,9,10};
         pairs(arr);
         System.out.println();
         pairs(arr2);
-        
     }
     
 }
