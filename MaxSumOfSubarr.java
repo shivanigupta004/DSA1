8313e3bb33747d4e5f852dc3266ad740ae6cac9e
Shivani Gupta <94shivani004@gmail.com>
2025-08-26 08:11:47 +0000

    Add brute-force max subarray sum logic in Java

--- MaxSumOfSubarr.java
+++ MaxSumOfSubarr.java
@@ -1,38 +1,31 @@
 public class MaxSumOfSubarr {
 
-        public static void subArray(int arr[]){
-            int count=0;
-            int min=Integer.MAX_VALUE;
-            int max=Integer.MIN_VALUE;
+        public static void subArray(int arr [] )
+        {
+            int currSum = 0;
+            int maxsum = Integer.MIN_VALUE;
 
-            for(int i=0;i<arr.length;i++)
+            for(int i = 0; i < arr.length; i++)
             {   
-                for(int j=i;j<arr.length;j++)
-                {   int sum=0;
-                    
-                     count++;
-                    for(int k=i;k<=j;k++)
+                for(int j = i; j < arr.length; j++)
+                {   
+                    currSum = 0;
+                    for(int k = i; k <= j; k++)
                     {
-                         System.out.print(arr[k]+" ");
-                         sum=sum+arr[k];
-                         
-                    }System.out.println();
-                    if(min>sum){
-                        min=sum;
-                     }
-                     if(max<sum){
-                        max=sum;
+                         currSum += arr[k];
+                    } 
+                    System.out.println("Current sum : " + currSum);
+                     if(maxsum < currSum){
+                        maxsum = currSum;
                     }
-                    System.out.println("sum of above array is "+sum);
-                    
-                }System.out.println();
+                }
+                System.out.println();
             }
-    
-            System.out.println("Total no of subarrays is "+count);
-            System.out.println("min sum of subarrays is "+min);
-            System.out.println("max sum of subarrays is "+max);
+            System.out.println("Maximun sum of subarrays : " + maxsum);
         }
-        public static void main(String args[]){
+        
+        public static void main(String args [] )
+        {
             int arr[]={2,4,6,8,10};
             subArray(arr);
         }
