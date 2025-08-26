e6ce084362139df91e2d47ec7a1b1202df2b6ae2
Shivani Gupta <94shivani004@gmail.com>
2025-08-26 05:46:14 +0000

    Add array reversal logic in Java

--- reverse.java
+++ reverse.java
@@ -1,9 +1,10 @@
 public class reverse {
-    public static void reverseA(int arr[]){
-        int first=0,last=arr.length-1;
+    public static void reverseA( int arr [] )
+    {
+        int first = 0, last = arr.length - 1;
 
-        while(first<last){
-            //swap
+        while ( first < last )
+        {   //swap
             int temp=arr[last];
             arr[last]=arr[first];
             arr[first]=temp;
@@ -12,13 +13,13 @@ public class reverse {
             last--;
         }
     }
-    public static void main(String args[]){
-        int arr[]={2,4,6,8,10,12,14,18 };
-        reverseA(arr);
-        for(int i=0;i<arr.length;i++){
+    public static void main( String args [] )
+    {
+        int arr [] = { 2,4,6,8,10,12,14,18 };
+        reverseA( arr );
+        for(int i = 0; i < arr.length; i++)
+        {
             System.out.print(arr[i]+" ");
         }
-
     }
-    
 }
