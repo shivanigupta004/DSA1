8a6cf686a82632d1142d7a4bc40308cde4e48477
Shivani Gupta <94shivani004@gmail.com>
2025-09-14 04:36:33 +0000

    Added descending order bubble sort method

--- bubblesort.java
+++ bubblesort.java
@@ -12,6 +12,19 @@ public class bubblesort {
         }
     }
 }
+ public static void bubblesortingInDescending(int[] arr) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
     public static void printArr(int[] arr) {
         for (int ele : arr) {
             System.out.print(ele + " ");
@@ -22,7 +35,7 @@ public class bubblesort {
         int arr[] = { 4, 5, 3, 2, 1 };
         System.out.print("Before Sorting arr : ");
         printArr(arr);
-        bubblesorting(arr);
+        bubblesortingInDescending(arr);
         System.out.print("After  Sorting arr : ");
         printArr(arr);
     }
