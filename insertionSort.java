d0d15e0659dd7c39a3d78c5a2b65a5e3f042d3f5
Shivani Gupta <94shivani004@gmail.com>
2025-09-14 04:55:04 +0000

    Added descending insertion sort

--- insertionSort.java
+++ insertionSort.java
@@ -12,16 +12,29 @@ public class insertionSort {
             arr[prev+1] = curr;
         }
     }
+    public static void insertionSortingInDesc(int[]arr){
+        for(int i = 0;i<=arr.length -1; i++){
+           int curr = arr[i];
+           int prev = i-1;
+           //finding out the correct pos to insert
+            while(prev >= 0 && arr[prev] < curr){
+                arr[prev+1] =arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev+1] = curr;
+        }
+    }
      public static void printArr(int[] arr) {
         for (int ele : arr) {
             System.out.print(ele + " ");
         }System.out.println();
     }
     public static void main(String[] args) {
-        int arr[] = {9,8,6,4,5,1};
+        int arr[] = {9,6,3,8,4,2,1};
         System.out.print("Before Sorting arr : ");
         printArr(arr);
-        insertionSorting(arr);
+        insertionSortingInDesc(arr);
         System.out.print("After  Sorting arr : ");
         printArr(arr);
        
